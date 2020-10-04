ef033dacef2cc0b3e628338ee607b6b8ddd9fbaa
chaoting <02115214@gm.scu.edu.tw>
2020-10-04 15:01:06 +0800

    1004

--- src/http_server2.js
+++ src/http_server2.js
@@ -5,7 +5,6 @@
  const server = http.createServer((req,res)=>{
 // src/http_server2.js // 注意非同步問題
 
-
     fs.writeFile(
         __dirname+'/../data/headers01.txt', 
         JSON.stringify(req.headers), 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const express = require('express');
+require('dotenv').config();  //管理資料庫帳密
 
+const express = require('express');  //引入 express
+const multer = require('multer');
+const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
+const axios = require('axios');
+const session = require('express-session');
+const MysqlStore = require('express-mysql-session')(session);
+const moment = require('moment-timezone');
+const db = require('./db_connect2');
+const sessionStore = new MysqlStore({}, db);
+const upload = multer({dest: __dirname + '/../tmp_uploads'});
+
+// 建立 web server 物件
 const app = express();
 
 // 註冊樣版引擎
@@ -7,64 +20,182 @@ app.set('view engine', 'ejs');
 // 設定views路徑 (選擇性設定)
 // app.set('views', __dirname + '/../views');
 
-// '/abc/deg' ，給兩層路由如何除錯？
+
+app.use( express.urlencoded({extended: false}));
+app.use( express.json() );
+// 設定session，顯示頁面刷新次數
+app.use(session({
+    saveUninitialized: false,
+    resave: false,
+    secret: 'jghdkasskjfks37848kj',
+    store: sessionStore,
+    cookie: {
+        maxAge: 1200000
+    }
+}));
+app.use((req, res, next)=>{
+    res.locals.title = '小新牛排店';
+    res.locals.sess = req.session;
+    next();
+})
+
+// 設定路由
 app.get('/', (req, res)=>{
     // res.send('<h2>Hola </h2>');
     res.render('home',{name: 'Shinder'});
 });
 
+
+// 透過ejs，以表格呈現JSON資料
 app.get('/json-sales', (req, res)=>{
     const sales = require(__dirname + '/../data/sales');
-    // res.send(sales.constructor.name)// 進到這裏面，自動變array
+    res.locals.title += ' - JSON';
+    // res.send(sales.constructor.name)//是個array
     // res.json(sales);// 告訴用戶端瀏覽器，這是json
     res.render('json-sales', {sales})
 });
 
+
 app.get('/json-sales2', (req, res)=>{
     const sales = require(__direname + '/../data/sales');
-    // 進到這裏面，自動變array
-    //res.send(sales.constructor.name)
     res.render('abc/def/json-sales2',{sales});
-    // 告訴用戶端瀏覽器，這是json
 });
 
-// 測試路由
+// 取得queryString的資料
 app.get('/try-qs', (req, res)=>{
     res.json(req.query);
 });
 
-const parser = express.urlencoded
-
 app.post('/try-post',(req, res)=>{
     res.json(req.body);
 });
 
-app.post('/try-post',(req, res)=>{
-    res.json(req.body);
+app.get('/try-post-form',(req, res)=>{
+    res.render('try-post-form');
 });
 
-// res.end(); //content-type是text
-// res.send();  //content-type是html
-// res.render();  // 樣板字串
-// res.json();  // 告訴用戶端瀏覽器，這是json
-// 都會去寫檔頭，只能有一個執行，否則會出錯
-// node程式啟動，會抓檔案進來?????
-// 第一次抓的檔案已經在記憶體裡頭，抓到的內容是當下的內容
+app.post('/try-post-form',(req, res)=>{
+    res.render('try-post-form', req.body);
+});
 
-app.use(express.static(__dirname + '/../public'))
+
+app.post('/try-upload', upload.single('avatar'), (req, res)=>{
+    console.log(req.file);
+
+    if(req.file && req.file.originalname){
+        let ext = '';
+
+        switch(req.file.mimetype){
+            case 'image/png':
+            case 'image/jpeg':
+            case 'image/gif':
+
+                fs.rename(
+                    req.file.path,
+                    __dirname + '/../public/img/' + req.file.originalname,
+                    error=>{
+
+                        return res.json({
+                            success: true,
+                            path: '/img/'+ req.file.originalname
+                        });
+                    });
+
+                break;
+            default:
+                fs.unlink(req.file.path, error=>{
+                    return res.json({
+                        success: false,
+                        msg: '不是圖檔'
+                    });
+                });
+        }
+    } else {
+        return res.json({
+            success: false,
+            msg: '沒有上傳檔案'
+        });
+    }
+});
+
+app.get('/try-uuid',(req, res)=>{
+    res.json({
+        uuid1: uuidv4(),
+        uuid2: uuidv4(),
+    });
+});
+
+const upload2 = require(__dirname + '/upload-img-module');
+app.post('/try-upload2', upload2.single('avatar'), (req, res)=> {
+    res.json(req.file);
+});
+
+app.get('/my-params1/:action?/:id?', (req, res)=> {
+    res.json(req.params);
+});
+app.get('/my-params2/*?/*?', (req, res)=> {
+    res.json(req.params);
+});
+
+app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res)=> {
+    let u = req.url.slice(3).split('?')[0];
+    u = u.replace(/-/g, '');
+    res.send(u);
+}); 
+
+app.use(require(__dirname + '/routes/admin2'));
+app.use('/members',require(__dirname + '/routes/admin3')); 
+
+app.get('/yahoo',async (req,res)=>{
+    const response = await axios.get('https://tw.yahoo.com/');
+})
+
+// session設定，顯示頁面刷新次數
+app.get('/try-session', (req, res)=>{
+    req.session.myVar = req.session.myVar || 0;
+    req.session.myVar++;
+    res.json({
+        myVar: req.session.myVar,
+        session: req.session
+    });
+});
+
+// 使用外掛moment
+app.get('/try-moment', (req, res)=>{
+    const fm = 'YYYY-MM-DD HH:mm:ss';
+    const now = moment(new Date());
+    res.json({
+    t1: new Date(),
+    t2: now.format(fm),
+    t2a: now.tz('Europe/London').format(fm),
+    t3: moment(req.session.cookie.expires).format(fm),
+    t3: moment(req.session.cookie.expires).tz('Asia/Tokyo').format(fm),
+    'process.env.DB_NAME':process.env.DB_NAME, 
+    });
+});    
+
+// 讀取MySQL資料庫中的資料表
+app.get('/try-db',(req,res)=>{
+    db.query('SELECT *FROM  address_book LIMIT 250')
+        .then(([results])=>{
+            res.json(results);
+        })
+});
+
+app.use('/address-book', require(__dirname + '/routes/address-book'));
+
 //使用靜態內容的資料夾
+app.use( express.static(__dirname + '/../public'));
 
-// app.use(express.static('public'));
-// 檔案在專案資料夾內，才有辦法啟動
-// 如果進入到src資料夾啟動，就會找不到檔案
-
+// 自訂404頁面
 app.use((req, res)=>{
     res
         .type('text/plain')
         .status(404)
-        .send('找不到網頁');
+        .send('找不到網頁啦XDDD');
 });
 
+// Server 偵聽
 app.listen(3000, ()=>{
     console.log('伺服器已啟動...');
 })
