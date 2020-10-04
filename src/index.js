46e1f00e1b87acd04edefaad33ecea4ef70fe8fe
chaoting <02115214@gm.scu.edu.tw>
2020-10-04 15:00:36 +0800

    1004

--- src/index.js
+++ src/index.js
@@ -7,12 +7,49 @@ app.set('view engine', 'ejs');
 // 設定views路徑 (選擇性設定)
 // app.set('views', __dirname + '/../views');
 
-app.get('/abc/def', (req, res)=>{
+// '/abc/deg' ，給兩層路由如何除錯？
+app.get('/', (req, res)=>{
     // res.send('<h2>Hola </h2>');
     res.render('home',{name: 'Shinder'});
 });
 
+app.get('/json-sales', (req, res)=>{
+    const sales = require(__dirname + '/../data/sales');
+    // res.send(sales.constructor.name)// 進到這裏面，自動變array
+    // res.json(sales);// 告訴用戶端瀏覽器，這是json
+    res.render('json-sales', {sales})
+});
 
+app.get('/json-sales2', (req, res)=>{
+    const sales = require(__direname + '/../data/sales');
+    // 進到這裏面，自動變array
+    //res.send(sales.constructor.name)
+    res.render('abc/def/json-sales2',{sales});
+    // 告訴用戶端瀏覽器，這是json
+});
+
+// 測試路由
+app.get('/try-qs', (req, res)=>{
+    res.json(req.query);
+});
+
+const parser = express.urlencoded
+
+app.post('/try-post',(req, res)=>{
+    res.json(req.body);
+});
+
+app.post('/try-post',(req, res)=>{
+    res.json(req.body);
+});
+
+// res.end(); //content-type是text
+// res.send();  //content-type是html
+// res.render();  // 樣板字串
+// res.json();  // 告訴用戶端瀏覽器，這是json
+// 都會去寫檔頭，只能有一個執行，否則會出錯
+// node程式啟動，會抓檔案進來?????
+// 第一次抓的檔案已經在記憶體裡頭，抓到的內容是當下的內容
 
 app.use(express.static(__dirname + '/../public'))
 //使用靜態內容的資料夾
